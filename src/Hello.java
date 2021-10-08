1db7c75f2a6bdb981ade900d12f06d5db8b48115
User <User@DESKTOP-GB3V6FU>
2021-10-08 10:57:22 +0530

    Second Commit

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello
 {
 public static void main(String[] args) 
 {
-System.out.println("hello");	
+System.out.println("hello world");	
 }
 }
 //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
